16fa83c4b9bcf0a623e45ed65aee9ba229c63419
Lanseria <zhangchao564265135@hotmail.com>
2016-04-11 20:36:46 +0800

    db type

--- db/bp.sql
+++ db/bp.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50626
 File Encoding         : 65001
 
-Date: 2016-04-10 17:07:10
+Date: 2016-04-11 20:35:58
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,7 +27,11 @@ CREATE TABLE `b_cart` (
   `cprice` float NOT NULL,
   `ccreate_time` datetime NOT NULL,
   PRIMARY KEY (`cid`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of b_cart
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for b_goods
@@ -49,7 +53,11 @@ CREATE TABLE `b_goods` (
   `gis_selloff` int(11) unsigned NOT NULL DEFAULT '0',
   `gview` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`gid`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of b_goods
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for b_message
@@ -62,7 +70,11 @@ CREATE TABLE `b_message` (
   `mcontent` text,
   `mcreate_time` datetime NOT NULL,
   PRIMARY KEY (`mid`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of b_message
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for b_type
@@ -75,6 +87,27 @@ CREATE TABLE `b_type` (
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of b_type
+-- ----------------------------
+INSERT INTO `b_type` VALUES ('1', '语言类');
+INSERT INTO `b_type` VALUES ('2', '理工科类');
+INSERT INTO `b_type` VALUES ('3', '文学类');
+INSERT INTO `b_type` VALUES ('4', '艺术类');
+INSERT INTO `b_type` VALUES ('5', '男生');
+INSERT INTO `b_type` VALUES ('6', '女生');
+INSERT INTO `b_type` VALUES ('7', '宝宝');
+INSERT INTO `b_type` VALUES ('8', '大叔');
+INSERT INTO `b_type` VALUES ('9', '笔记本');
+INSERT INTO `b_type` VALUES ('10', '通讯类');
+INSERT INTO `b_type` VALUES ('11', '摄影');
+INSERT INTO `b_type` VALUES ('12', '配件');
+INSERT INTO `b_type` VALUES ('13', '电影票');
+INSERT INTO `b_type` VALUES ('14', '演唱会门票');
+INSERT INTO `b_type` VALUES ('15', '免费票');
+INSERT INTO `b_type` VALUES ('16', '其他票');
+INSERT INTO `b_type` VALUES ('17', 'Other');
+
+-- ----------------------------
 -- Table structure for b_user
 -- ----------------------------
 DROP TABLE IF EXISTS `b_user`;
@@ -85,7 +118,11 @@ CREATE TABLE `b_user` (
   `upwd` varchar(255) NOT NULL,
   `uavatar` varchar(255) NOT NULL DEFAULT 'avatars/avatar.png',
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of b_user
+-- ----------------------------
 
 -- ----------------------------
 -- View structure for b_vcart
